7eb3dc8bc2e1708382553ea81e9c498e4c6dd6d2
SenyorTerry <37777549+SenyorTerry@users.noreply.github.com>
2019-02-25 09:43:16 +0100

    Update Main.java
    Código final lectura XML

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,14 @@
  */
 package Main;
 
+import org.w3c.dom.Document;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import java.io.File;
+
 /**
  *
  * @author santi.molto
@@ -15,9 +23,43 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola Mundo");
-        System.out.println("Primera Modificacion");
+        
+        try {
+            //Path File
+            File fXmlFile = new File("C:\\Users\\santi.molto\\Desktop\\documento.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
+
+            NodeList nList = doc.getElementsByTagName("Usuario");
+
+            System.out.println("----------------------------");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+
+                Node nNode = nList.item(temp);
+
+                System.out.println("Current Element: " + nNode.getNodeName());
+
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element eElement = (Element) nNode;
+
+                    System.out.println("\nID: " + eElement.getAttribute("id"));
+                    System.out.println("Nombre: " + eElement.getElementsByTagName("Nombre").item(0).getTextContent());
+                    System.out.println("Apellidos: " + eElement.getElementsByTagName("Apellidos").item(0).getTextContent());
+                    System.out.println("Edad: " + eElement.getElementsByTagName("Edad").item(0).getTextContent());
+                    System.out.println("Profesion: " + eElement.getElementsByTagName("Profesion").item(0).getTextContent());
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
 }
+
